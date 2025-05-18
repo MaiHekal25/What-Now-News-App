00e5740d7f14d7475d5712f0d025aa6b82260f25
Mai Hekal <maihekal25@gmail.com>
2025-05-18 08:17:53 +0300

    Implement API request for news articles
    This commit introduces the functionality to fetch news articles from the NewsAPI.
    
    Key changes include:
    - Initializing Retrofit with a base URL and Gson converter.
    - Adding an `HttpLoggingInterceptor` for debugging network requests.
    - Defining an `INews` interface (not shown in diff but implied) for the API endpoint.
    - Making an asynchronous call to the `getNews()` endpoint.
    - Logging the retrieved articles or an error message in `onResponse` and `onFailure` respectively.
    - Utilizing ViewBinding for `ActivityMainBinding`.

--- app/src/main/java/com/training/whatnow/MainActivity.kt
+++ app/src/main/java/com/training/whatnow/MainActivity.kt
@@ -1,14 +1,60 @@
 package com.training.whatnow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.training.whatnow.databinding.ActivityMainBinding
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    //https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=a894df1ef11f4c25b9951037b52924e4&pageSize=30
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadNews()
+    }
+
+    private fun loadNews() {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        val retrofit = Retrofit
+            .Builder()
+            .baseUrl("https://newsapi.org")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+
+
+        val c = retrofit.create(INews::class.java)
+        c.getNews().enqueue(object : Callback<News> {
+            override fun onResponse(
+                call: Call<News?>,
+                response: Response<News?>
+            ) {
+                val news = response.body()
+                var articles = news?.articles
+                Log.d("###", "Articles:$articles ")
+            }
+
+            override fun onFailure(
+                call: Call<News?>,
+                t: Throwable
+            ) {
+                Log.d("###", "Error: Articles Are Not Available")
+            }
+
+        })
     }
 }
